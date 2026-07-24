0fa09ff2c1cf38d2df0f86298b4f7aa979100265
Emma Olofsson <emma@haileyhr.com>
2026-07-24 14:58:13 +0200

    fixed add to calender

--- src/pages/Home/components/AddToCalendarButton.tsx
+++ src/pages/Home/components/AddToCalendarButton.tsx
@@ -1,34 +1,23 @@
 import { Button } from '@mantine/core'
 import { IconCalendarPlus } from '@tabler/icons-react'
 import { useLanguage } from '../../../i18n/LanguageContext'
-import { downloadCalendarEvent } from '../../../lib/calendar'
-import { WEDDING } from '../data/wedding'
 import classes from './AddToCalendarButton.module.css'
+
+const calendarHref = `${import.meta.env.BASE_URL}emma-philip-brollop.ics`
 
 export function AddToCalendarButton() {
   const { t } = useLanguage()
 
-  const handleClick = () => {
-    downloadCalendarEvent({
-      title: t('calendarTitle', {
-        partnerOne: WEDDING.partnerOne,
-        partnerTwo: WEDDING.partnerTwo,
-      }),
-      description: t('calendarDescription'),
-      location: `${WEDDING.venue}, ${WEDDING.city}`,
-      start: WEDDING.ceremonyDate,
-      end: WEDDING.ceremonyEnd,
-    })
-  }
-
   return (
     <Button
       className={classes.button}
+      component="a"
+      href={calendarHref}
+      download="emma-philip-brollop.ics"
       variant="outline"
       radius="xl"
       size="md"
       leftSection={<IconCalendarPlus size={18} stroke={1.5} />}
-      onClick={handleClick}
     >
       {t('addToCalendar')}
     </Button>
